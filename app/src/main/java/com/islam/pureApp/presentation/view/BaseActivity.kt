0fdaf2f3f7e6c3f67b7bc03dec525f8db1dc9313
islam.ar <islam.ahmed@approcks.com>
2022-05-26 08:48:02 +0200

    main screen UI

---
+++ app/src/main/java/com/islam/pureApp/presentation/view/BaseActivity.kt
@@ -0,0 +1,36 @@
+package com.islam.pureApp.presentation.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseActivity<viewBinding : ViewBinding> : AppCompatActivity() {
+
+    private var _binding: viewBinding? = null
+    private val binding: viewBinding
+        get() {
+            return _binding ?: throw IllegalStateException(
+                "data binding should not be requested before onCreate is called"
+            )
+        }
+
+    abstract val bindingInflater: (LayoutInflater) -> viewBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        _binding = bindingInflater.invoke(layoutInflater)
+        setContentView(_binding?.root)
+
+        setupOnCreate()
+    }
+
+    abstract fun setupOnCreate()
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
---
+++ app/src/main/java/com/islam/pureApp/presentation/view/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.islam.pureApp.presentation.view
+
+import android.app.SearchManager
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.SearchView
+import com.islam.pureApp.R
+import com.islam.pureApp.databinding.ActivityMainBinding
+
+private const val TAG = "MainActivity"
+
+class MainActivity : BaseActivity<ActivityMainBinding>() {
+
+    override val bindingInflater: (LayoutInflater) -> ActivityMainBinding
+        get() = ActivityMainBinding::inflate
+
+    override fun setupOnCreate() {
+        Log.d(TAG, "setupOnCreate")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.search_menu, menu)
+
+        val searchManager = getSystemService(SEARCH_SERVICE) as SearchManager
+        val searchView: SearchView =
+            menu?.findItem(R.id.startSearch)?.actionView as SearchView
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+
+                Log.d(TAG, "onQueryTextSubmit: ")
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+
+                Log.d(TAG, "onQueryTextChange: ")
+                return true
+            }
+        })
+
+        searchView.setOnCloseListener {
+            searchView.onActionViewCollapsed()
+            true
+        }
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.startSearch -> {
+                Log.d(TAG, "start Search")
+            }
+            R.id.sortList -> {
+                Log.d(TAG, "Sort List")
+
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+}
