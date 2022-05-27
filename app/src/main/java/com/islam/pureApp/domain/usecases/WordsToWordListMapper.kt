1b61c9bf77c936566cb5cfbbea772564f61d11be
islam.ar <islam.ahmed@approcks.com>
2022-05-27 21:25:13 +0200

    mapper refactor

--- app/src/main/java/com/islam/pureApp/domain/usecases/WordsToWordListMapper.kt
+++ app/src/main/java/com/islam/pureApp/domain/usecases/WordsToWordListMapper.kt
@@ -6,38 +6,14 @@ import org.jsoup.Jsoup
 class WordsToWordListMapper {
 
     fun map(body: String): List<Word> {
-        val regex = "[^A-Za-z]".toRegex()
         val text = Jsoup.parse(body).body().text()
-
-        val wordWithCountMap = createWordsMap(text, regex)
-        return createWordList(wordWithCountMap)
-    }
-
-    private fun createWordsMap(
-        text: String,
-        regex: Regex,
-    ): MutableMap<String, Int> {
-        val wordWithCountMap = mutableMapOf<String, Int>()
-        text.trim().split(" ").forEach { word ->
-            if (word.isNotEmpty() && word.isNotBlank()) {
-                val key = regex.replace(word, "")
-                wordWithCountMap[key] = wordWithCountMap[word]?.plus(1) ?: 1
-            }
-        }
-        return wordWithCountMap
-    }
-
-    private fun createWordList(wordWithCountMap: MutableMap<String, Int>): ArrayList<Word> {
-        val wordList = arrayListOf<Word>()
-        wordWithCountMap.onEachIndexed { index, entry ->
-            wordList.add(
-                Word(
-                    id = index, text = entry.key,
-                    count = entry.value
-                )
-            )
-        }
-        return wordList
+        return text
+            .split("\\b".toRegex())
+            .filter { it.any { char -> char.isLetterOrDigit() } }
+            .groupingBy { it }
+            .eachCount()
+            .map { it }
+            .mapIndexed { index, textCount -> Word(index + 1, textCount.key, textCount.value) }
     }
 
 }
