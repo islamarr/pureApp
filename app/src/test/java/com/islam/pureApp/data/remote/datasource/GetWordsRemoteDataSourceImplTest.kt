2d432a18fc082f907b018b07e30e77537c27fb07
islam.ar <islam.ahmed@approcks.com>
2022-05-28 16:02:31 +0200

    remote sata source unit testing

---
+++ app/src/test/java/com/islam/pureApp/data/remote/datasource/GetWordsRemoteDataSourceImplTest.kt
@@ -0,0 +1,72 @@
+package com.islam.pureApp.data.remote.datasource
+
+import com.islam.pureApp.data.remote.api.ApiService
+import com.islam.pureApp.data.remote.api.NetworkResponse
+import com.islam.pureApp.data.remote.api.Response
+import com.islam.pureApp.getOrAwaitValue
+import org.junit.Assert.*
+
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+
+class GetWordsRemoteDataSourceImplTest {
+
+    private lateinit var remoteDataSource: GetWordsRemoteDataSourceImpl
+
+    @Mock
+    private lateinit var apiService: ApiService
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.openMocks(this)
+        remoteDataSource = GetWordsRemoteDataSourceImpl(apiService)
+    }
+
+    @Test
+    fun `when response is success return the right NetworkResponse`(){
+        val networkResponse = Response.Success(200, "response")
+        Mockito.`when`(apiService.getAllWords()).thenReturn(networkResponse)
+
+        val actual = remoteDataSource.getWordsResponse()
+        val expected = NetworkResponse.Success(networkResponse.value)
+
+        assertEquals(actual, expected)
+    }
+
+    @Test
+    fun `when response has Error return Failure NetworkResponse with error message`(){
+        val networkResponse = Response.Error(400, "error")
+        Mockito.`when`(apiService.getAllWords()).thenReturn(networkResponse)
+
+        val actual = remoteDataSource.getWordsResponse()
+        val expected = NetworkResponse.Failure<String>(networkResponse.message)
+
+        assertEquals(actual, expected)
+    }
+
+    @Test
+    fun `when response has EmptyError return Failure NetworkResponse with empty message`(){
+        val networkResponse = Response.EmptyError
+        Mockito.`when`(apiService.getAllWords()).thenReturn(networkResponse)
+
+        val actual = remoteDataSource.getWordsResponse()
+        val expected = NetworkResponse.Failure<String>("")
+
+        assertEquals(actual, expected)
+    }
+
+    @Test
+    fun `when response has NetworkError return Failure NetworkResponse with null message`(){
+        val networkResponse = Response.NetworkError
+        Mockito.`when`(apiService.getAllWords()).thenReturn(networkResponse)
+
+        val actual = remoteDataSource.getWordsResponse()
+        val expected = NetworkResponse.Failure<String>(null)
+
+        assertEquals(actual, expected)
+    }
+
+}
